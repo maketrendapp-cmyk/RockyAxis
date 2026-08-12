4ba1c71390a29caed46c6381b4a659ba1a004bcc
maketrendapp-cmyk <maketrendapp@gmail.com>
2026-08-12 22:06:02 +0545

    Update _app.js

--- rocky-axis/pages/_app.js
+++ rocky-axis/pages/_app.js
@@ -1,7 +1,17 @@
+// pages/_app.js
 import '../styles/globals.css';
+import Header from '../components/rockyaxis/Header';
+import Footer from '../components/rockyaxis/Footer'; // optional – if you want a global footer too
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      {/* If you have a global footer, place it here */}
+      {/* <Footer /> */}
+    </>
+  );
 }
 
 export default MyApp;
