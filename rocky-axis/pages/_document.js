406a1a60abedec9dd580e94b9dc4204e1de54a82
maketrendapp-cmyk <maketrendapp@gmail.com>
2026-08-12 16:37:49 +0545

    Create pages/_document.js

---
+++ rocky-axis/pages/_document.js
@@ -0,0 +1,21 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Preconnect to CDN for performance */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        {/* Global favicon – ensure you have one */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
