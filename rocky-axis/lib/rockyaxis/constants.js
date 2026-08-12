ab05829d49900e2494d8edb775bb33cebdd39419
maketrendapp-cmyk <maketrendapp@gmail.com>
2026-08-12 13:42:38 +0545

    Create lib/rockyaxis/constants.js

---
+++ rocky-axis/lib/rockyaxis/constants.js
@@ -0,0 +1,8 @@
+export const CATEGORIES = ['All', 'Regedit', 'Headshot Configs', 'Sensi APKs', 'GFX Tools', 'Audio', 'Packs'];
+export const PLATFORMS = ['All', 'Android', 'iOS', 'Windows', 'PC'];
+export const SORT_OPTIONS = [
+  { value: 'popular', label: 'Popular' },
+  { value: 'rating', label: 'Top Rated' },
+  { value: 'newest', label: 'Newest' },
+  { value: 'featured', label: 'Featured' },
+];
